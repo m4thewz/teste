59f5a887912a63d5ab70de3f0009a480aa64640a
Matheus Vitor <m4theusdev@gmail.com>
2023-05-01 14:12:02 -0300

    moved paths

--- src/router.js
+++ src/router.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Home from '@/views/Home'
+import About from '@/views/About'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home,
   },
   {
