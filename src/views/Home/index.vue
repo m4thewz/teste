f5f70a41358c5f8e3de4a4f008eeb82b9d8bc06c
Matheus Vitor <m4theusdev@gmail.com>
2023-05-01 18:49:50 -0300

    home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="home">
-    Matheus
+  <div id="home">
+    <main>
+      <section>
+        <div>
+          <img class="pic" src="@/assets/foto.png"/>
+        </div>
+        <div>
+          <p>Um programador freelancer que gosta de fazer sites</p>
+          <hr>
+          <ul>
+            <li>
+              <a href="https://github.com/m4thewz" target="_blank" title="Github">
+                <img src="@/assets/github.svg" alt="Github">
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/matheuspach/" target="_blank" title="Linkedin">
+                <img src="@/assets/linkedin.svg" alt="Linkedin">
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/m4thewzs" target="_blank" title="Instagram">
+                <img src="@/assets/instagram.svg" alt="Instagram">
+              </a>
+            </li>
+            <li>
+              <a href="mailto:m4theusdev@gmail.com" target="_blank" title="Email">
+                <img src="@/assets/mail.svg" alt="Email">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
+<style lang="scss" scoped src="@/views/Home/style.scss"></style>
